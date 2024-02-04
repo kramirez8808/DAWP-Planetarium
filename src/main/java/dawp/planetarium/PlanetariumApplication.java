dc2619daa810a0438e1272cc87eaafcdced0bd10
Kevin R <kramirez8808@gmail.com>
2024-02-04 13:32:46 -0600

    Primer Commit: HTML y CSS
    Página completa. Se publica para llevar el control de versiones antes de refactorizar el código.

---
+++ src/main/java/dawp/planetarium/PlanetariumApplication.java
@@ -0,0 +1,13 @@
+package dawp.planetarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetariumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dawp/planetarium/PlanetariumApplicationTests.java
@@ -0,0 +1,13 @@
+package dawp.planetarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
